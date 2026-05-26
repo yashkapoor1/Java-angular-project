1fa6f9c4fc32a33c3f9ff61505bcefa4c7a8e3c9
yashkapoor1 <yashkapoor72@gmail.com>
2026-05-26 16:36:28 +0530

    Added test insert query (dummy data) for Insert_Query_EFile.sql in "MySQL Query" folder.

---
+++ MySQL Query/Insert_Query_EFile.sql
@@ -0,0 +1,22 @@
+use testdemoapp;
+
+
+INSERT INTO e_file
+(
+`diary_no`,
+`reg_no`,
+`title_name`,
+`state_id`,
+`state`,
+`district_id`,
+`district`,
+`file_name`,
+`file_path`
+)
+VALUES
+('1234', '56463', 'bharat', '1', 'Haryana', '2', 'Sonepat', 'a.pdf' , '/uploads/');
+
+
+
+
+
